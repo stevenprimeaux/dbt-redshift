704e42d4224d5ca17af6c373eca94f53f654efb5
Steven Primeaux <stevenprimeaux@gmail.com>
2024-07-02 21:17:07 +0000

    Add dim_customers.

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,6 +1,1 @@
-select
-    id as customer_id,
-    first_name,
-    last_name
-
-from dev.jaffle_shop.customers
+select id as customer_id, first_name, last_name from dev.jaffle_shop.customers
--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,7 +1,2 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
-
+select id as order_id, user_id as customer_id, order_date, status
 from dev.jaffle_shop.orders
